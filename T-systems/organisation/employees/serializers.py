208e271c1c5ae4946f63bbd82ff467b824ad40e6
jyotikawade <jkawade2000@gmail.com>
2022-02-17 15:21:22 +0530

    added app users

--- T-systems/organisation/employees/serializers.py
+++ T-systems/organisation/employees/serializers.py
@@ -1,58 +1,32 @@
-
 from rest_framework import serializers
 from .models import Employee
 
 
 class EmployeeSerializer(serializers.ModelSerializer):
-
     """
     class - EmployeeSerializer
 
-    methods
-    -------
+    inner class - Meta
 
-    create:
-    revoked while post request
-    update:
-    revoked while put request
     """
 
     class Meta:
         model = Employee
         fields = ['eno', 'ename', 'esal', 'eaddr']
 
-    def create(self, validated_data):
+    def validate_eno(self, value):
         """
-            parameter -
-            ----------
-            self:
-            The self parameter is a reference to the current instance of the class
+        field level validation
 
-            validated_data:
-            object for validation
-
-        """
-        return Employee.objects.create(**validated_data)
-                #Employee is table name
-
-    def update(self, instance, validated_data):
-        """
         parameter -
         ----------
         self:
         The self parameter is a reference to the current instance of the class
 
-        instance :
-        old data stored in database
-
-        validated data:
-        new data from user for updation
+        value :
+        value for validation
 
         """
-
-        instance.eno = validated_data.get('eno', instance.eno)
-        instance.ename = validated_data.get('ename', instance.ename)
-        instance.esal = validated_data.get('esal', instance.esal)
-        instance.eaddr = validated_data.get('eaddr', instance.eaddr)
-        instance.save()
-        return instance
+        if value >= 50000:
+            raise serializers.ValidationError('invalid eno')
+        return value
--- T-systems/organisation/users/serializers.py
+++ T-systems/organisation/users/serializers.py
@@ -6,15 +6,8 @@ from .models import Users
 class UserSerializer(serializers.ModelSerializer):
 
     """
-    class - EUserSerializer
-
-    methods
-    -------
-
-    create:
-    revoked while post request
-    update:
-    revoked while put request
+    class - UserSerializer
+    inner class - Meta
 
     """
 
@@ -22,37 +15,20 @@ class UserSerializer(serializers.ModelSerializer):
         model = Users
         fields = ['user_no', 'user_name', 'user_app']
 
-    def create(self, validated_data):
-        """
-            parameter -
-            ----------
-            self:
-            The self parameter is a reference to the current instance of the class
 
-            validated_data:
-            object for validation
-
+    def validate_user_no(self,value):
         """
-        return Users.objects.create(**validated_data)
-                #Users is table name
+        field level validation for user_no
 
-    def update(self, instance, validated_data):
-        """
         parameter -
         ----------
         self:
         The self parameter is a reference to the current instance of the class
 
-        instance :
-        old data stored in database
-
-        validated data:
-        new data from user for updation
+        value :
+        value for validation
 
         """
-
-        instance.user_no = validated_data.get('user_no', instance.user_no)
-        instance.user_name = validated_data.get('user_name', instance.user_name)
-        instance.user_app = validated_data.get('user_app', instance.user_app)
-        instance.save()
-        return instance
+        if value>=5000:
+            raise serializers.ValidationError('to many users')
+        return value
